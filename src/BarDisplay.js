51bf6602ba42c0f97230b9f13d1eb2ab1b9166e3
Unkvlt <sabrerechelle@gmail.com>
2019-11-17 22:49:39 -0500

    Update BarDisplay.js

--- src/BarDisplay.js
+++ src/BarDisplay.js
@@ -13,9 +13,24 @@ export default class BarDisplay {
         this.graphData = graphData;
         this.buildChart();
     }
+    
     //starting to build the chart
     buildChart() {
+    let data = this.graphData;
+    console.log(data);
 
+    let seasonsName = 0;
+    seasonsName = parseInt(seasonsName);
+    console.log(seasonsName);
+
+    let dataset = data.seasons[seasonsName].seasonData;
+    //this is the fall data
+    let dataset1 = data.seasons[0].seasonData;
+    console.log(dataset1);
+
+    //winter data
+    let dataset2 = data.seasons[1].seasonData;
+    console.log(dataset2);
     // set the width and height of the svg
     //create the svg as the graph
     let svg = d3.select(this.barHolder)
@@ -23,15 +38,20 @@ export default class BarDisplay {
         //add extra height to show negative values
         .attr("height", this.h);
     
+    
     //get the precip and year values from data.json
-    let precipData = this.graphData.data.map(d=>d.precip);
-    let yearData = this.graphData.data.map(d=>d.year);
+    let precipData = dataset.map(d=>d.precip);
+    console.log(precipData);
+
+    let yearData = dataset.map(d=>d.year);
+    console.log(yearData);   
 
     //add the scale to the x axis
     let xScale = d3.scaleBand()
         .domain(yearData)
         .range([0,1200])
-        .paddingInner(0.05);
+        .paddingInner(0.07);
+        
         
     // add the y scale/numbers to the y axis
     let yScale = d3.scaleLinear()
@@ -48,13 +68,18 @@ export default class BarDisplay {
         .ticks(20);
         // .tickFormat(d => d + ' Precip');
 
+    // let fallButton = d3.selectAll('body').append('button').text('fall').on('click').data(dataset1).remove().buildChart();
+    // let winterButton = d3.selectAll('body').append('button').text('winter').on('click').data(dataset2).remove().buildChart();
+
+
+
     // append the new group of the rect (bars)
     svg.append('g')
         .attr('transform', 'translate(30, 0)')
         //create the rects
         .selectAll("rect")
             //usse the data
-            .data(this.graphData.data)
+            .data(data.seasons[0].seasonData)
             .enter()
             .append("rect")
             //the width position
@@ -77,14 +102,14 @@ export default class BarDisplay {
             .attr("fill",d => {
                 if (d.precip > 0) {
                     //change it to a brighter colours - pinks
-                    return `#CE3883`;
+                    return `#C35300`;
                     
                 } else {
                     //if its less than 0 change the colour to darker colours - orange/fall
-                    return `rgb(${Math.abs(d.precip)*1},201,161)`;
+                    return `rgb(${Math.abs(d.precip)*1},131,121)`;
                 }
             });
-
+    
     // append the group of labels 
     svg.append('g')
         //create the classes for the labels
@@ -92,12 +117,13 @@ export default class BarDisplay {
         //get all the text
         .selectAll("text")
             //get the data
-            .data(this.graphData.data)
+            .data(data.seasons[seasonsName].seasonData)
             .enter()
             //make the text go to the proper positions
             .append("text")
                 //move the text over to be on top the bar - the middle of the bar
                 .attr('transform', 'translate(60, 0)')
+                .attr("align-content","center")
                 //make the text be the label of the precipitation its on
                 .text(d => d.precip)
                 //get the x attribute to be 10 higher than the bar
@@ -112,9 +138,11 @@ export default class BarDisplay {
                     }
                 })
                 //change the fill of the labels
-                .attr("fill", "#000")
+                .attr("fill", "black")
                 //change the font size
-                .attr("font-size", "17px");
+                .attr("font-size", "15px")
+                //change the font-weight for visual purposes
+                .attr("font-weight", "700");
     
     //define the min data for precip value
     let minVal = d3.min(precipData);
@@ -130,9 +158,10 @@ export default class BarDisplay {
         //add the prescription for xscale
         .append("text")
             .attr('transform', 'translate(600,40)')
-            .attr("font-size", "18px")
+            .attr("font-size", "23px")
             .style("text-anchor", "end")
-            .style("fill", "#454545")
+            .style("fill", "#0A8379")
+            .attr("font-weight","600")
             .text("Year");
         
 
@@ -144,46 +173,45 @@ export default class BarDisplay {
         //add the prescription for yscale
         .append("text")
             .attr("transform", "rotate(-90), translate(-500,-40)")
-            .attr("font-size", "18px")
+            .attr("font-size", "23px")
             .style("text-anchor", "start")
-            .style("fill", "#454545")
+            .style("fill", "#0A8379")
+            .attr("font-weight","600")
             .text("Precipitation (%)");
     
     //add legend for precipitation
     //background
     svg.append("rect")
-        .attr("x",100).
-        attr("y", 5)
-        .attr("width", 250)
-        .attr("height", 80)
-        .attr("fill","#cdcdcd")
+        .attr("x",100)
+        .attr("y", 5)
+        .attr("width", 300)
+        .attr("height", 85)
+        .attr("fill","#E5E5E5")
     //2 small circles
     svg.append("circle")
         .attr("cx",125)
         .attr("cy",30)
         .attr("r", 6)
-        .style("fill", "#ff4dbe")
+        .style("fill", "#C35300")
     svg.append("circle")
         .attr("cx",125)
         .attr("cy",60)
         .attr("r", 6)
-        .style("fill", "#01c9a1")
+        .style("fill", "#0D8379")
     //text display
     svg.append("text")
         .attr("x", 150)
-        .attr("y", 30)
+        .attr("y", 35)
         .text("Positive Precipitation (%)")
-        .style("font-size", "15px")
+        .style("font-size", "20px")
         .attr("alignment-baseline","middle")
     svg.append("text")
         .attr("x", 150)
-        .attr("y", 60)
+        .attr("y", 70)
         .text("Negative Precipitation (%)")
-        .style("font-size", "15px")
+        .style("font-size", "20px")
         .attr("alignment-baseline","middle")
-    
     //END OF START CHART
     }
-
  // END OF BAR DISPLAY
 }
